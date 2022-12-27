de754dbe5f24037dfd677e0a5e1629d31adc32d3
Marat Field <mrpolyansky@yandex.ru>
2022-12-27 22:55:27 +0300

    New branch junit4-platform (JUnit4, и Ñ and JUnit Jupiter)

--- src/test/java/ru/netology/service/CashBackHackerTest.java
+++ src/test/java/ru/netology/service/CashBackHackerTest.java
@@ -1,14 +1,17 @@
 package ru.netology.service;
 
-import org.junit.Test;
-
+//import org.junit.Test;
 import static org.junit.Assert.assertEquals;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
+
 
 public class CashBackHackerTest {
 
     @Test
-    public void shouldRemainIfLess1000() {
-        // подготовка данных, выполнение целевого действия, сравнение ожидаемого и фактического
+    public void shouldRemainIfLess1000V() {
+
         CashBackHacker cashBackHacker = new CashBackHacker();
         int amount = 900;
 
@@ -19,8 +22,8 @@ public class CashBackHackerTest {
     }
 
     @Test
-    public void shouldRemainIfMore1000() {
-        // подготовка данных, выполнение целевого действия, сравнение ожидаемого и фактического
+    public void shouldRemainIfMore1000V() {
+
         CashBackHacker cashBackHacker = new CashBackHacker();
         int amount = 1900;
 
@@ -31,8 +34,8 @@ public class CashBackHackerTest {
     }
 
     @Test
-    public void shouldNotRemainIfEquals1000() {
-        // подготовка данных, выполнение целевого действия, сравнение ожидаемого и фактического
+    public void shouldNotRemainIfEquals1000V() {
+
         CashBackHacker cashBackHacker = new CashBackHacker();
         int amount = 1000;
 
@@ -40,5 +43,41 @@ public class CashBackHackerTest {
         int expected = 0;
 
         assertEquals(expected, actual);
+    }
+
+    @Test
+    void shouldRemainIfLess1000J() {
+
+        CashBackHacker cashBackHacker = new CashBackHacker();
+        int amount = 900;
+
+        int actual = cashBackHacker.remain(amount);
+        int expected = 100;
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    void shouldRemainIfMore1000J() {
+
+        CashBackHacker cashBackHacker = new CashBackHacker();
+        int amount = 1900;
+
+        int actual = cashBackHacker.remain(amount);
+        int expected = 100;
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    void shouldNotRemainIfEquals1000J() {
+
+        CashBackHacker cashBackHacker = new CashBackHacker();
+        int amount = 1000;
+
+        int actual = cashBackHacker.remain(amount);
+        int expected = 0;
+
+        Assertions.assertEquals(expected, actual);
     }
 }
